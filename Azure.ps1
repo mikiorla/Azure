592e3be30f446531016035b5a8e41eac731ffd51
Milan Orlovic <mikiorla@gmail.com>
2017-09-28 11:02:18 +0200

    Update Azure.ps1

--- Azure.ps1
+++ Azure.ps1
@@ -18,7 +18,7 @@ Import-Module $AZUREmodulepath
 
 
 $KT_subscription = '1d3a2dce-8063-48fe-af1f-22c062ee0555'
-$cred = Get-Credential -UserName milan@kt.rs -Message 'Azure user MILAN'
+$cred = Get-Credential 
 Add-AzureAccount -Credential $cred
 
 Get-AzureAccount | fl *
